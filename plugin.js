57a2b52891ee73feafe0598503c21d6f8f80cb05
msn <rpserjo@gmail.com>
2024-02-22 16:29:38 +0300

    commit

--- plugin.js
+++ plugin.js
@@ -1,21 +1,7 @@
 (function () {
     'use strict';
 
-    var premium_plugin = {
-        name: 'Premium plugin',
-        version: '0.0.1',
-        description: 'Fake premium'
-    };
-
-    Lampa.Settings.listener.follow('open', function (e) {
-        console.log('FakePremium', e)
-        if (e.name == 'fake_prem') {
-            e.body.find('[data-parent="proxy"]').remove();
-        }
-    });
-
-    console.log('FakePremium', 'started, enabled:', Lampa.Storage.field('fake_premium'));
-    console.log('Lampa', Lampa)
+    console.log('FakePremium', 'started');
     Lampa.Account.hasPremium = () => {
         return true
     }
